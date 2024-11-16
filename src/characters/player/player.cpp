14cc4124dc1d4bda7bca208a46da1a50f0b7bc8f
Long Vu <15876242+longv@users.noreply.github.com>
2024-11-16 11:36:22 +0200

    Move player with animations

--- src/characters/player/player.cpp
+++ src/characters/player/player.cpp
@@ -1,5 +1,10 @@
 #include "player.h"
+#include "godot_cpp/classes/animation_player.hpp"
+#include "godot_cpp/classes/input.hpp"
+#include "godot_cpp/classes/sprite3d.hpp"
 #include "godot_cpp/variant/utility_functions.hpp"
+#include "godot_cpp/variant/vector2.hpp"
+#include "godot_cpp/variant/vector3.hpp"
 #include <godot_cpp/core/class_db.hpp>
 
 using namespace godot;
@@ -8,14 +13,40 @@ void Player::_bind_methods() {
 }
 
 Player::Player() {
-	// Initialize any variables here.
-	time_passed = 0.0;
+	direction = Vector2(0, 0);
 }
 
 Player::~Player() {
 	// Add your cleanup here.
 }
 
-void Player::_physics_process(double delta) {
+void Player::_ready() {
+	sprite = get_node<Sprite3D>("Sprite3D");
+	animation_player = get_node<AnimationPlayer>("AnimationPlayer");
+
+	animation_player->play("Idle");
+}
+
+void Player::_input(const Ref<InputEvent> &p_event) {
+	direction = Input::get_singleton()->get_vector(
+			"MoveLeft",
+			"MoveRight",
+			"MoveForward",
+			"MoveBackward");
+
+	if (direction == Vector2(0, 0)) {
+		animation_player->play("Idle");
+	} else {
+		animation_player->play("Move");
+	}
+}
+
+void Player::_physics_process(double p_delta) {
 	// UtilityFunctions::print("Player process");
+	Vector3 velocity = Vector3(direction.x, 0, direction.y);
+	velocity *= 5;
+
+	set_velocity(velocity);
+
+	move_and_slide();
 }
--- src/characters/player/player.h
+++ src/characters/player/player.h
@@ -1,7 +1,11 @@
 #ifndef GDEXAMPLE_H
 #define GDEXAMPLE_H
 
-#include <godot_cpp/classes/character_body3d.hpp>
+#include "godot_cpp/classes/animation_player.hpp"
+#include "godot_cpp/classes/character_body3d.hpp"
+#include "godot_cpp/classes/input_event.hpp"
+#include "godot_cpp/classes/sprite3d.hpp"
+#include "godot_cpp/variant/vector2.hpp"
 
 namespace godot {
 
@@ -9,16 +13,22 @@ class Player : public CharacterBody3D {
 	GDCLASS(Player, CharacterBody3D)
 
 private:
-	double time_passed;
+	Sprite3D *sprite = nullptr;
+	AnimationPlayer *animation_player = nullptr;
+
+	Vector2 direction;
 
 protected:
-	static void _bind_methods();
+	static void
+	_bind_methods();
 
 public:
 	Player();
 	~Player();
 
-	void _physics_process(double delta) override;
+	void _ready() override;
+	void _input(const Ref<InputEvent> &p_event) override;
+	void _physics_process(double p_delta) override;
 };
 
 } //namespace godot
